acc848f42f08cb4994557e9e98a88d4020636f5a
slorello89 <slorello89@gmail.com>
2019-10-18 11:53:15 -0400

    first commit

---
+++ MessagesTensorFlow/Controllers/InboundController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagesTensorFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InboundController : ControllerBase
+    {
+        [HttpPost]
+        public HttpStatusCode Post()
+        {
+            return HttpStatusCode.OK;
+        }
+    }
+}
---
+++ MessagesTensorFlow/TFEngine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MessagesTensorFlow
+{
+    public class TFEngine
+    {
+        static readonly string _assetsPath = Path.Combine(Environment.CurrentDirectory, "assets");
+        static readonly string _imagesFolder = Path.Combine(_assetsPath, "train");
+        static readonly string _savePath = Path.Combine(_assetsPath, "predict");
+        static readonly string _trainTagsTsv = Path.Combine(_imagesFolder, "tags.tsv");
+        static readonly string _inceptionTensorFlowModel = Path.Combine(_assetsPath, "inception5h", "tensorflow_inception_graph.pb");        
+
+        static readonly object _lock = new object();
+        
+        private static TFEngine _instance;
+        public static TFEngine Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new TFEngine();
+                    }
+                    return _instance;
+                }
+
+            }
+        }
+    }
+}
